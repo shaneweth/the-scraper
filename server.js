01412c0c5a48c9780445d9a3d6bb30714310df72
willow <shaneweth77@gmail.com>
2019-06-12 13:06:47 -0400

    filled out boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+const PORT = 3000; 
+
+// Initialize Express
+const app = express();
+
+// Middlest of Wares!
+
+// Morgan for the logs...
+app.use(logger("dev"));
+// Parse JSON req body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Static Public Folder
+app.use(express.static("public"));
+
+// Must Connect to Mongo
+mongoose.connect("add URL", { useNewUrlParser: true });
+
+// ROUTES
+
+//GET to the website...
+app.get("/scrape", function(req, res) {
+    // axios to get the html body
+    axios.get("https://www.example.com").then(function(response) {
+        // Load into CHEERIO and save to $ as a shorthand selector
+        const $ = cheerio.load(response.data);
+    })
+})
